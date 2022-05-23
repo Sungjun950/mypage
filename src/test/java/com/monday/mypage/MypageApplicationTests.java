dfb32ecd54fa2e48d03dc7ff532b0847c9ded5fe
Kim <ayayryey@khu.ac.kr>
2022-05-23 15:31:01 +0900

    init

---
+++ src/test/java/com/monday/mypage/MypageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.monday.mypage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MypageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
